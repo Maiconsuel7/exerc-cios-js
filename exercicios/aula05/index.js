d4124f25dd204a460bae6ff2efd7f4944e18b454
Maiconsuel7 <msps3@aluno.ifnmg.edu.br>
2026-04-09 20:14:50 -0300

    Update index.js

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -7,52 +7,52 @@
 
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
-  if (idade < 12)
-    return "criança"
-  else if (idade >= 12 && idade <= 17)
+  if (idade < 12) {
+    return "crianca"
+  } else if (idade >= 12 && idade <= 17) {
     return "adolescente"
-  else (idade >= 18)
-  return "adulto"
+  } else {
+    return "adulto"
+  }
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
-  const desconto = preco * (percentual / 100)
-  const precoFinal = preco - desconto
-  return precoFinal
+  return preco - (preco * percentual / 100)
 }
-
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
-  if (nota >= 60)
+ if (nota >= 60) {
     return "aprovado"
-  else if (nota >= 40 && nota < 60)
+  } else if (nota >= 40 && nota < 60) {
     return "recuperação"
-  else (nota < 40)
-  return "reprovado"
+  } else {
+    return "reprovado"
+  }
 }
+
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
-  let soma = 0;
+  let soma = 0
 
   for (let numero of numeros) {
     if (numero % 2 === 0) {
-      soma += numero;
+      soma += numero
     }
   }
 
-  return soma;
+  return soma
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
-  let contador = 0;
+  let contador = 0
 
   for (let caractere of texto) {
     if (caractere === letra) {
-      contador++;
+      contador++
     }
   }
 
-  return contador;
+  return contador
 }
